93394524f4dcba187a39d1abb517c449af3a5c68
Luis Segoviano <the.segoviano@gmail.com>
2021-12-03 12:05:59 -0600

    Initial Commit

---
+++ Falabella Technical Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Falabella Technical Test
+//
+//  Created by Luis Segoviano on 03/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
